2b2a9da9d0dd2fa05831c2762f28d827d2e1eb19
Vasanth <64586431+Vasanthengineer4949@users.noreply.github.com>
2023-03-01 15:32:02 +0530

    Update data_api.py

--- utils/data_api.py
+++ utils/data_api.py
@@ -3,7 +3,7 @@ import json
 
 # API_KEY = "4dbc17e007ab436fb66416009dfb59a8"
 def NewsData(): 
-    main_url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=4dbc17e007ab436fb66416009dfb59a8"
+    main_url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3d7d2570874a4de697f837fc650bb57b"
     open_bbc_page = requests.get(main_url).json()
     article = open_bbc_page["articles"]
     with open("news_dict.json", "w") as f:
